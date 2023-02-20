fefd5fc00c9eb5f7415b73cc0ff052f8976aa75e
Omkar Sanjiv Shelke (EHG Intern) <omkar.shelke@eh.com>
2023-02-20 16:41:54 +0400

    marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,32 +1,69 @@
+
 import { useEffect, useRef, useState } from "react";
 import * as tt from '@tomtom-international/web-sdk-maps'
-import './Map.css';
+import './Map.css'
+import'@tomtom-international/web-sdk-maps/dist/maps.css'
 
+/******************-Imports*************************** */
 export const Map = () =>{
-
     const[map,setMap]= useState({});
-    const[lat,setlat]=useState(12.9716);
-    const[lng,setlng]=useState(77.5946);
+    const[lat,setLat]=useState(12.9716);
+    const[lng,setLng]=useState(77.5946);
     const mapELement =useRef();
-
+/******************-API-integration-*************************** */
     useEffect(()=>{
         let map = tt.map({
-          key:'B6G6zRswUJMuH5VueWbHX7I3zEyaqkU8',
+          key:process.env.REACT_APP_TOM_TOM_API_KEY,
           container:mapELement.current,
+          stylesVisibility:{
+            trafficFlow:true,
+            trafficIncidents:true
+          },
           center:[lng,lat],
           zoom:14,
         })
         setMap(map)
-      },[])
 
+        const element = document.createElement('div');
+        element.className= 'marker';
+        const addMarker =()=>{
+          const marker = new tt.Marker({
+            draggable:true,
+            element:element
+          })
+          .setLngLat([lng,lat])
+          .addTo(map)
+          marker.on('dreagend',()=>{
+            const lngLat = marker.getLngLat()
+            setLng(lngLat.lng)
+            setLat(lngLat.lat)
+          })
+        }
+        addMarker();
+        return ()=> map.remove()
+      },[lng,lat])
+
+/******************-Return-File-*************************** */
     return ( 
      <>
         <div className="input container">
-            <input type='text' placeholder="Longitude"/>
-            <input type='text' placeholder="Latitude"/>
+            <input 
+              type='text' 
+              placeholder="longitude"
+              id='longitude'
+              className="longitude"
+              onChange={(e)=>{setLng(e.target.value)}}
+            />
+            <input 
+              type='text' 
+              placeholder="latitude"
+              id='latitude'
+              className="latitude"
+              onChange={(e)=>{setLat(e.target.value)}}
+            />
         </div>
         <div className="map-container">
-            <div ref={mapELement}></div>
+            <div ref={mapELement} className="map-container"></div>
         </div>
      </> 
      )
